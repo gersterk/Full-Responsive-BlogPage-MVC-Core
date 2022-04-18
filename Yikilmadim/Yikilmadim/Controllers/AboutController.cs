ae0b971591639966ca72061ed618af2c1987cd33
gersterk <barisdogan.biz@gmail.com>
2022-04-18 23:07:34 +0200

    About
    Creating the About Section of the page... Adding a service, manager, controller, view and other needs...

---
+++ Yikilmadim/Yikilmadim/Controllers/AboutController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using DataAccesLayer.EntityFramework;
+
+
+namespace Yikilmadim.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager abm = new AboutManager(new EFAboutRepository());
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult SocialMediaAbout()
+        {
+            var values = abm.GetList();
+
+            return PartialView(values);
+
+        }
+    }
+}
