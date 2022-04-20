1623402dc22b443fb9f13cd7f0432ebf70885bee
gersterk <barisdogan.biz@gmail.com>
2022-04-20 17:09:18 +0200

    enhancement...

--- Yikilmadim/Yikilmadim/ViewComponents/Blog/BlogLast3Post.cs
+++ Yikilmadim/Yikilmadim/ViewComponents/Blog/BlogLast3Post.cs
@@ -1,6 +1,18 @@
-﻿namespace Yikilmadim.ViewComponents.Blog
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Yikilmadim.ViewComponents.Blog
 {
-    public class BlogLast3Post
+    public class BlogLast3Post : ViewComponent
     {
+        BlogManager bm = new BlogManager(new EFBlogRepository());
+
+
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetLast3Blog();
+            return View(values);
+        }
     }
 }
